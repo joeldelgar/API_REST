f388456d787dffd33fef7a530e91f336fbec2f37
JoanaOP <joana.orpella@gmail.com>
2022-06-25 12:28:11 +0200

    Register modifications location

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -48,12 +48,12 @@ class AuthRoutes {
   }
 
   public async register (req: Request, res: Response) {
+    console.log(req.body)
     const { name, surname, username, password, phone, mail, languages, photo, role } = req.body
     const salt = await bcrypt.genSalt(10)
     const hashed = await bcrypt.hash(password, salt)
     console.log('1')
-    console.log(req.body.location[0])
-    const location = { type: 'Point', coordinates: req.body.location, index: '2dsphere' }
+    const location = { type: 'Point', coordinates: [req.body.location.coordinates[0], req.body.location.coordinates[1]], index: '2dsphere' }
     console.log('2')
     const newUser = new User({ name, surname, username, password: hashed, phone, mail, languages, location, photo, active: true, fromGoogle: false })
     console.log('3')
